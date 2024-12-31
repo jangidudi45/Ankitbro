d6017fd68ff486e4e1c2fc57b11cf261555571e8
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-31 09:37:01 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,10 @@
-# config.py
-BOT_NAME = "Free bot"
-BOT_TOKEN = "6755775439:AAGkahjp3xK71u-jG6V0uQUR-xJgqLPt9yw"
-API_ID = 28328736
-API_HASH = "802254a44896baa87f3083b7af36b2e5"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+#Ankit Shakya
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
